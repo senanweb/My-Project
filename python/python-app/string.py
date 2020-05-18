61065b25d5a015ba0ba4f977d913ba3ed353b1c5
senanweb <senansalawe@gmail.com>
2020-05-19 01:25:59 +0200

    code-3

--- python/python-app/string.py
+++ python/python-app/string.py
@@ -1,12 +1,12 @@
-x = "sinan" # string
-y1 = 10 # number
-print(x, y1) #print
-print(id(x)) #print sting and number
+x = "sinan"  # string
+y1 = 10  # number
+print(x, y1)  # print
+print(id(x))  # print sting and number
 print(hex(id(x)))
-#--test2
+# --test2
 x1 = 'sinan'
 x2 = "nadom"
-print (x1.lower() + " " + x2.upper())
-#--Program to access to ram
+print(x1.lower() + " " + x2.upper())
+# --Program to access to ram
 sw = 's' * 50
-print (sw)
+print(sw)
