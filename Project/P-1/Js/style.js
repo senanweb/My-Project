47f74a6536dff26e1b1ad048642995b710c322f3
senanweb <senansalawe@gmail.com>
2020-05-23 00:01:00 +0200

    code-5 and if in python

---
+++ Project/P-1/Js/style.js
@@ -0,0 +1,21 @@
+// select land-page
+//  let landing = document.querySelector('.land-page');
+//get array images
+// let myimages = ['3.jpg', '4.jpg', '5.jpg', '6.jpg'];
+//change background imgs
+//landing.style.backgroundImage = 'url ("../Images/4.jpg")';
+//time for change random
+//let randomNumber = Math.floor(Math.random() * myimages.length );
+//check the console
+//console.log(randomNumber);
+//  setInterval(() => {
+//   let randomNumber = Math.floor(Math.random() * myimages.length);
+//   console.log(randomNumber);
+//  }, 1000);
+// ================================
+// let landing = document.querySelector('.land-page');
+// let myimages = ['3.jpg','4.jpg','5.jpg','6.jpg'];
+// setInterval(() => {
+//   let randomNumber = Math.floor(Math.random() * myimages.length);
+//   landing.style.backgroundImage ='url ("Images/' + myimages[randomNumber] + '")';
+// }, 1000);
