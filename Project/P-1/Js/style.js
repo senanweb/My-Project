380c64a5d28b56ae95f4b408d01570917a15f954
senanweb <senansalawe@gmail.com>
2020-05-28 20:32:53 +0200

    fun-and-web

--- Project/P-1/Js/style.js
+++ Project/P-1/Js/style.js
@@ -1,21 +1,29 @@
-// select land-page
-//  let landing = document.querySelector('.land-page');
-//get array images
-// let myimages = ['3.jpg', '4.jpg', '5.jpg', '6.jpg'];
-//change background imgs
-//landing.style.backgroundImage = 'url ("../Images/4.jpg")';
-//time for change random
-//let randomNumber = Math.floor(Math.random() * myimages.length );
-//check the console
-//console.log(randomNumber);
-//  setInterval(() => {
-//   let randomNumber = Math.floor(Math.random() * myimages.length);
-//   console.log(randomNumber);
-//  }, 1000);
-// ================================
-// let landing = document.querySelector('.land-page');
-// let myimages = ['3.jpg','4.jpg','5.jpg','6.jpg'];
-// setInterval(() => {
-//   let randomNumber = Math.floor(Math.random() * myimages.length);
-//   landing.style.backgroundImage ='url ("Images/' + myimages[randomNumber] + '")';
-// }, 1000);
+// gear change setting
+var myToggle = document.querySelector(".toggle .fa-cog").onclick = function(){
+  this.classList.toggle("fa-spin"); //this for fa
+//toggle open
+var myOpen = document.querySelector(".box").classList.toggle("open");
+}
+//change color
+const colorLi = document.querySelectorAll (".color li");
+colorLi.forEach(li => {
+  //for click
+  li.addEventListener("click", (e) => { 
+      // console.log(e.target.dataset.color) //print code for color
+      //set-color
+      document.documentElement.style.setProperty('--main--color', e.target.dataset.color);
+    });
+});
+// gear change setting
+// ====================== change the background 
+let landing = document.querySelector(".land-page");
+let myimages = ["../Images/naruto.jpg",
+                "../Images/wall-2.jpg",
+                "../Images/sunrise.jpg",
+                "../Images/wall.jpg"];
+setInterval(() => {
+ let randomNumber = Math.floor(Math.random()* myimages.length);
+ landing.style.backgroundImage = 'url("Images/' + myimages[randomNumber] + '")';
+ console.log(randomNumber);
+},5000);
+// =====================================================
---
+++ python/python-app/functtion.py
@@ -0,0 +1,16 @@
+#functions
+def mark():
+    print('===================')
+def inserTion(sle1,sel2):
+    print(int(sle1) + int(sel2))  
+mark()  #function
+x = 'sinan';
+print (x)
+mark() #function
+y = 43
+print (y)
+mark() #function
+s1 = input ('please insert your number : ')
+s2 = input ('please insert your number : ')
+inserTion(s1,s2) #function
+mark() #function
